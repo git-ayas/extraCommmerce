bee8d1be99a22f9e8d08c71f5d59a50f8d074e0b
Ayaskant Mishra <ayas.optimark@gmail.com>
2021-08-28 20:51:19 +0530

    Working fetch api call
    - state deep copying added to mutator/reducer for accurate state updates
    - loading products on page load with useEffect hook

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,5 +1,5 @@
 import { IonButton, IonButtons, IonCol, IonContent, IonHeader, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { StoreContext } from '../stores/StoreContext';
 import './Page.css';
@@ -7,9 +7,28 @@ import './Page.css';
 const Page: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
-  const { state, action } = useContext(StoreContext);
-  console.log(state, action)
- 
+  const { state: storeState, action: storeAction } = useContext(StoreContext);
+  const { fetchedProducts } = storeState
+  useEffect(() => {
+    console.log("Mounted")
+    storeAction('GET')
+    return () => {
+      //cleanup
+
+    }
+  },[storeState.fetchedProducts.length])
+
+  const productListings = fetchedProducts && fetchedProducts.length > 0
+    ? fetchedProducts.map((product: any, index: any) => {
+      return (
+        <IonRow key={`product_row_${index}_${Date.now() * Math.floor(Math.random() * 1024)}`}>
+          <IonCol>{product.name}</IonCol>
+          <IonCol>{product.price}</IonCol>
+          <IonCol>{product.description}</IonCol>
+        </IonRow>
+      )
+    })
+    : null
 
   return (
     <IonPage>
@@ -29,23 +48,15 @@ const Page: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonRow>
-
-          <IonCol></IonCol>
-          <IonCol></IonCol>
-
-        </IonRow>
-        <IonRow>
           <IonCol>
             <IonButton onClick={() => {
-              action('GET', null)
+              storeAction('GET')
             }}>
-              Fetch Products
+              {storeState.status === "fetching" ? "Fetching" : "Fetch Products"}
             </IonButton>
           </IonCol>
-          <IonCol>
-            {JSON.stringify(state)}
-          </IonCol>
         </IonRow>
+        {productListings}
       </IonContent>
     </IonPage>
   );
--- src/stores/StoreContext.tsx
+++ src/stores/StoreContext.tsx
@@ -2,35 +2,47 @@ import React, { createContext, useReducer } from "react";
 import storeModel from "../models/storeModel";
 
 interface StateType {
-    fetchedProducts: any
+    status: "idle" | "fetching",
+    fetchedProducts: any,
 }
-let initialState: StateType = { fetchedProducts: {} }
+let initialState: StateType = { status: "idle", fetchedProducts: {} }
 interface ActionType { type: string, payload: any }
 let dummy: any = {}
 let StoreContext = createContext(dummy)
 const StoreDataController = new storeModel();
 const mutator = (state: StateType, action: ActionType) => {
+    let immutableState = state
 
     switch (action.type) {
         case "updateFetchedProducts":
-            state.fetchedProducts = action.payload
+            immutableState.fetchedProducts = action.payload
             break;
+        case "statusUpdate":
+            immutableState.status = action.payload
+            break
 
         default:
             break;
     }
-    return state
+    return { ...state, ...immutableState }
 
 }
 
 function StoreProvider(props: any) {
     const [state, dispatch] = useReducer(mutator, initialState)
 
-    const action = (name: any, payload: any) => {
+    const action = (name: any, payload: any = null) => {
         switch (name) {
             case "GET":
+                dispatch({
+                    type: "statusUpdate", payload: "fetching"
+                })
                 StoreDataController.getProducts().then((res) => {
+                    console.log("checking state", state)
                     dispatch({ type: "updateFetchedProducts", payload: res.data })
+                    dispatch({
+                        type: "statusUpdate", payload: "idle"
+                    })
                 })
 
                 break;
